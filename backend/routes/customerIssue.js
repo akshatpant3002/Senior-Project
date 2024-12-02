58d4f0438589bc6094ec342bfec0641f77231a76
akshatpant3002 <akshatpant3002@gmail.com>
2024-12-02 18:30:33 -0500

    edit issue done

--- backend/routes/customerIssue.js
+++ backend/routes/customerIssue.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { submitIssue, removeIssues, getAllIssues } = require('../controllers/customerIssueController');
+const { submitIssue, removeIssues, getAllIssues, editIssue } = require('../controllers/customerIssueController');
 
 router.post('/submitIssue', submitIssue);
 
 router.delete('/deleteIssue', removeIssues);
 
 router.get('/issues', getAllIssues);
+
+router.post('/editIssue', editIssue);
 
 module.exports = router;
